9f2c72c80e4fff06f1cc2e7d1aab39b89a314a7a
Jaieden <this_is_bob12@hotmail.com>
2017-05-28 18:37:00 +1000

    Jaieden arduino codes and notes

---
+++ Jaieden Working/Jaiedens arduino/sketch_may27a/sketch_may27a.ino
@@ -0,0 +1,88 @@
+int latchPin = 10;
+int clockPin = 11;
+int dataPin = 9;
+
+int currentPoints = 0;
+
+int incomingByte = 0;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Starting...");
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  startUpSequence();
+}
+
+void startUpSequence(){
+  const int delayTime = 150;
+  updateShiftRegister(0);
+  delay(delayTime);
+  updateShiftRegister(1);
+  delay(delayTime);
+  updateShiftRegister(3);
+  delay(delayTime);
+  updateShiftRegister(7);
+  delay(delayTime);
+  updateShiftRegister(15);
+  delay(delayTime);
+  updateShiftRegister(63);
+  delay(delayTime);
+  updateShiftRegister(127);
+  delay(delayTime);
+  updateShiftRegister(255);
+  delay(delayTime);
+  updateShiftRegister(0);
+  delay(delayTime);
+  updateShiftRegister(255);
+  delay(delayTime);
+  updateShiftRegister(0);
+  delay(delayTime);
+  updateShiftRegister(255);
+  delay(delayTime);
+  updateShiftRegister(0);
+  delay(delayTime);
+  updateShiftRegister(255);
+  delay(delayTime);
+  updateShiftRegister(0);
+  delay(delayTime);
+  updateShiftRegister(255);
+  delay(delayTime);
+  updateShiftRegister(0);
+  delay(delayTime);
+}
+
+int power(int value, int y){
+  if(y==0){
+    return 1;
+  }else{
+    return value * power(value, y-1);
+  }
+}
+
+void loop(){
+  if(Serial.available() > 0){
+    incomingByte = Serial.read() - 53;
+    Serial.print(" I received: ");
+    Serial.println(incomingByte);
+    if(currentPoints+incomingByte > 6){
+      currentPoints=6;
+    } else if(currentPoints+incomingByte < 0){
+      currentPoints = 0;
+    } else{
+      currentPoints += incomingByte;
+    }
+    
+    
+    //delay(10);
+  }
+  updateShiftRegister(power(2,currentPoints)-1);
+}
+
+void updateShiftRegister(int number){
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, number);
+  digitalWrite(latchPin, HIGH);
+}
+
---
+++ Jaieden Working/Matt_s_arduino/Matt_s_arduino.ino
@@ -0,0 +1,300 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
+int led5 = 6;
+int led6 = 7;
+char myCol[20];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (9600); 
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+/**digitalWrite(led1, HIGH);
+delay(1000);
+digitalWrite(led1, LOW);
+digitalWrite(led2, HIGH);
+delay(1000);
+digitalWrite(led2, LOW);**/
+
+  int lf = 10;
+  Serial.readBytesUntil(lf, myCol, 1);
+  
+  //Stretch 1 Blinking
+  while(strcmp(myCol,"a")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, HIGH);
+
+       delay(600);
+
+       digitalWrite(led6, LOW);
+
+
+       break;
+  }
+  
+  //Stretch 1 Solid
+   if(strcmp(myCol,"1")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, HIGH);  
+   }
+
+  //Stretch 2 Blinking
+  while(strcmp(myCol,"b")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, HIGH);
+       digitalWrite(led6, LOW);
+
+       delay(600);
+
+       digitalWrite(led5, LOW);
+
+
+       break;
+  }
+  
+   //Stretch 2 Solid
+   if(strcmp(myCol,"2")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, HIGH);
+       digitalWrite(led6, LOW);   
+   }
+
+   //Stretch 3 Blinking
+  while(strcmp(myCol,"c")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, HIGH); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);
+
+       delay(600);
+
+       digitalWrite(led2, LOW);
+
+
+       break;
+  }
+  
+   //Stretch 3 Solid
+   if(strcmp(myCol,"3")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, HIGH); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);    
+   }
+
+  //Stretch 4 Blinking
+  while(strcmp(myCol,"d")==0){
+       digitalWrite(led1, HIGH);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);
+
+       delay(600);
+
+       digitalWrite(led1, LOW);
+
+
+       break;
+  }
+  
+   //Stretch 4 Solid
+   if(strcmp(myCol,"4")==0){
+       digitalWrite(led1, HIGH);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);    
+   }
+
+  //Stretch 5 Blinking
+  while(strcmp(myCol,"e")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, HIGH); 
+       digitalWrite(led3, HIGH);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);
+
+       delay(600);
+
+       digitalWrite(led2, LOW);
+       digitalWrite(led3, LOW);
+
+
+       break;
+  }
+   //Stretch 5 Solid
+   if(strcmp(myCol,"5")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, HIGH); 
+       digitalWrite(led3, HIGH);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);    
+   }
+
+  //Stretch 6 Blinking
+  while(strcmp(myCol,"f")==0){
+       digitalWrite(led1, HIGH);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, HIGH);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);
+
+       delay(600);
+
+       digitalWrite(led1, LOW);
+       digitalWrite(led4, LOW);
+
+
+       break;
+  }
+  
+   //Stretch 6 Solid
+   if(strcmp(myCol,"6")==0){
+       digitalWrite(led1, HIGH);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, HIGH);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);    
+   }
+
+    //Stretch 7 Blinking
+  while(strcmp(myCol,"g")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, HIGH);
+       digitalWrite(led6, HIGH);
+
+       delay(600);
+
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);
+
+
+       break;
+  }
+  
+   //Stretch 7 Solid
+   if(strcmp(myCol,"7")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, HIGH);
+       digitalWrite(led6, HIGH);    
+   }
+
+     //Stretch 8 Blinking
+  while(strcmp(myCol,"h")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, HIGH); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, HIGH);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);
+
+       delay(600);
+
+       digitalWrite(led2, LOW);
+       digitalWrite(led4, LOW);
+
+
+       break;
+  }
+  
+   //Stretch 8 Solid
+   if(strcmp(myCol,"8")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, HIGH); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, HIGH);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);    
+   }
+
+     //Stretch 9 Blinking
+  while(strcmp(myCol,"i")==0){
+       digitalWrite(led1, HIGH);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, HIGH);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);
+
+       delay(600);
+
+       digitalWrite(led1, LOW);
+       digitalWrite(led3, LOW);
+
+
+       break;
+  }
+  
+   //Stretch 9 Solid
+   if(strcmp(myCol,"9")==0){
+       digitalWrite(led1, HIGH);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, HIGH);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);    
+   }
+
+   //Lights Off
+   if(strcmp(myCol,"0")==0){
+       digitalWrite(led1, LOW);       
+       digitalWrite(led2, LOW); 
+       digitalWrite(led3, LOW);
+       digitalWrite(led4, LOW);
+       digitalWrite(led5, LOW);
+       digitalWrite(led6, LOW);    
+   }
+
+   if(strcmp(myCol,"x")==0){
+       Serial.write("z"); 
+   }
+   
+}
